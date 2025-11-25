e23e1dc88004b5558a05b38d1611bfb63a587ace
June Park <joshypark0818@gmail.com>
2025-11-24 19:25:51 -0800

    wiener and richardson lucy docstring added

--- wiener.py
+++ wiener.py
@@ -2,6 +2,11 @@ from skimage import color, restoration
 import numpy as np
 
 def wiener_deconv(img):
+    '''
+    Performs unsupervised Wiener deconvolution on the input image. Splits color images into HSV channels and performs deconvolution on V.
+    
+    :param img: Image as np array RGB (H, W, 3)
+    '''
     assert img.dtype == np.float32 or img.dtype == np.float64, "Input image must be float32 or float64"
     psf = np.ones((5,5), np.float32) / 25
     psf /= psf.sum()
